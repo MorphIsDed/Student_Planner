9586ac7c0810c38fd711512dc51ffecf34d39c0e
Abhinay <sahuabhinay317@gmail.com>
2025-11-15 11:12:27 +0530

    Fixing the home file

---
+++ app/src/main/java/com/example/studentplanner/ui/navigation/Navigation.kt
@@ -0,0 +1,17 @@
+package com.example.studentplanner.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.studentplanner.ui.view.HomeScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen()
+        }
+    }
+}
