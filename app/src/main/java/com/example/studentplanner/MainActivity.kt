9eb6ade5385d6c05a23852ea2e1a091e35add1ef
Abhinay <sahuabhinay317@gmail.com>
2025-11-15 11:20:16 +0530

    First screen with LazyView

--- app/src/main/java/com/example/studentplanner/MainActivity.kt
+++ app/src/main/java/com/example/studentplanner/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.studentplanner.ui.navigation.AppNavigation
 import com.example.studentplanner.ui.theme.StudentPlannerTheme
+import com.example.studentplanner.ui.view.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StudentPlannerTheme {
-                AppNavigation()
+                HomeScreen()
             }
         }
     }
